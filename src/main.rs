393a7945f64c82c1b0a85e7131d2d2ccd5510549
Kenta Kudo <yd37574@gmail.com>
2020-11-18 22:06:52 +0000

    tokenise

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,132 @@
 use std::env;
-use std::process;
 
-fn main() {
+#[derive(Debug, PartialEq)]
+enum TokenKind {
+    Reserved(char),
+    Num(i64),
+    EOF,
+}
+
+#[derive(Debug, PartialEq)]
+struct Token {
+    kind: TokenKind,
+    next: Option<Box<Token>>,
+}
+
+impl Token {
+    fn consume(&self, op: char) -> Option<&Token> {
+        if self.kind != TokenKind::Reserved(op) {
+            return None;
+        }
+
+        self.next.as_ref().map(|t| t.as_ref())
+    }
+
+    fn consume_number(&self) -> Option<(i64, &Token)> {
+        if let TokenKind::Num(nr) = self.kind {
+            if let Some(next) = self.next.as_ref() {
+                return Some((nr, next.as_ref()));
+            }
+        }
+
+        None
+    }
+
+    fn is_eof(&self) -> bool {
+        return self.kind == TokenKind::EOF;
+    }
+
+    fn append(&mut self, kind: TokenKind) -> &mut Token {
+        let t = Token { kind, next: None };
+        self.next = Some(Box::new(t));
+        self.next.as_mut().unwrap()
+    }
+}
+
+fn tokenise(p: &str) -> Result<Token, String> {
+    let mut head = Token {
+        kind: TokenKind::EOF,
+        next: None,
+    };
+    let mut cur = &mut head;
+
+    let mut input = p;
+    while input != "" {
+        let c = input.chars().nth(0).unwrap();
+        if c.is_ascii_whitespace() {
+            input = &input[1..];
+            continue;
+        }
+
+        if c == '+' || c == '-' {
+            input = &input[1..];
+            cur = cur.append(TokenKind::Reserved(c));
+            continue;
+        }
+
+        if c.is_ascii_digit() {
+            let (d, rest) = strtol(input);
+            cur = cur.append(TokenKind::Num(
+                d.ok_or(format!("予期しない文字です: {}", input))?,
+            ));
+            input = rest;
+            continue;
+        }
+
+        return Err("トークナイズできません".to_string());
+    }
+
+    cur.append(TokenKind::EOF);
+
+    Ok(match head.next {
+        Some(t) => *t,
+        None => head,
+    })
+}
+
+fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        eprintln!("引数の個数が正しくありません");
-        process::exit(1);
+        return Err("引数の個数が正しくありません".to_string());
     }
+
+    let token = tokenise(&args[1])?;
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
 
-    let input = &args[1];
-    let (d, mut input) = match strtol(&input) {
-        (Some(d), input) => (d, input),
-        (None, input) => {
-            eprintln!("予期しない文字です: '{}'", input);
-            process::exit(1);
-        }
-    };
+    let (d, mut token) = token
+        .consume_number()
+        .ok_or("数ではありません".to_string())?;
     println!("  mov rax, {}", d);
 
-    while input != "" {
-        if input.chars().nth(0).unwrap() == '+' {
-            input = &input[1..];
-            let (d, rest) = match strtol(input) {
-                (Some(d), rest) => (d, rest),
-                (None, rest) => {
-                    eprintln!("予期しない文字です: '{}'", rest);
-                    process::exit(1);
+    while !token.is_eof() {
+        let next = match token.consume('+') {
+            Some(next) => {
+                let (d, next) = next
+                    .consume_number()
+                    .ok_or("数ではありません".to_string())?;
+                println!("  add rax, {}", d);
+                next
+            }
+            None => match token.consume('-') {
+                Some(next) => {
+                    let (d, next) = next
+                        .consume_number()
+                        .ok_or("数ではありません".to_string())?;
+                    println!("  sub rax, {}", d);
+                    next
                 }
-            };
-            println!("  add rax, {}", d);
-            input = rest;
-            continue;
-        }
+                None => return Err(format!("'{}'ではありません", '-')),
+            },
+        };
 
-        if input.chars().nth(0).unwrap() == '-' {
-            input = &input[1..];
-            let (d, rest) = match strtol(input) {
-                (Some(d), rest) => (d, rest),
-                (None, rest) => {
-                    eprintln!("予期しない文字です: '{}'", rest);
-                    process::exit(1);
-                }
-            };
-            println!("  sub rax, {}", d);
-            input = rest;
-            continue;
-        }
-
-        eprintln!("予期しない文字です: '{}'", input);
-        process::exit(1);
+        token = next;
     }
 
     println!("  ret");
-    process::exit(0);
+    Ok(())
 }
 
 fn strtol(s: &str) -> (Option<i64>, &str) {
@@ -87,5 +157,44 @@ mod tests {
         assert_eq!((None, "-4"), strtol("-4"));
         assert_eq!((Some(4), ""), strtol("4"));
         assert_eq!((None, ""), strtol(""));
+    }
+
+    #[test]
+    fn successfully_tokenise() -> Result<(), String> {
+        assert_eq!(
+            Token {
+                kind: TokenKind::Num(12),
+                next: Some(Box::new(Token {
+                    kind: TokenKind::Reserved('+'),
+                    next: Some(Box::new(Token {
+                        kind: TokenKind::Num(34),
+                        next: Some(Box::new(Token {
+                            kind: TokenKind::Reserved('-'),
+                            next: Some(Box::new(Token {
+                                kind: TokenKind::Num(5),
+                                next: Some(Box::new(Token {
+                                    kind: TokenKind::EOF,
+                                    next: None,
+                                }))
+                            }))
+                        }))
+                    }))
+                }))
+            },
+            tokenise(" 12 + 34 - 5 ")?
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn tokenise_empty() -> Result<(), String> {
+        assert_eq!(
+            Token {
+                kind: TokenKind::EOF,
+                next: None,
+            },
+            tokenise("  ")?
+        );
+        Ok(())
     }
 }
