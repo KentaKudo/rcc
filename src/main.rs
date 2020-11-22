414146c89c8f7b65fa91105bb8b4aa714675b0ec
Kenta Kudo <yd37574@gmail.com>
2020-11-22 10:07:57 +0000

    parser(2) – stack machine

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,7 @@ struct Token {
 }
 
 impl Token {
-    fn consume_reserved(&self) -> Option<(char, &Token)> {
+    fn consume_reserved(&self) -> Option<(char, &Self)> {
         if let TokenKind::Reserved(c) = self.kind {
             return Some((c, self.next.as_ref().unwrap().as_ref()));
         }
@@ -49,14 +49,14 @@ impl Token {
         None
     }
 
-    fn expect_reserved(&self) -> Result<(char, &Token)> {
+    fn expect_reserved(&self) -> Result<(char, &Self)> {
         self.consume_reserved().ok_or(CustomError(
             "有効な文字ではありません".to_string(),
             self.loc,
         ))
     }
 
-    fn consume_number(&self) -> Option<(i64, &Token)> {
+    fn consume_number(&self) -> Option<(i64, &Self)> {
         if let TokenKind::Num(nr) = self.kind {
             return Some((nr, self.next.as_ref().unwrap().as_ref()));
         }
@@ -64,16 +64,12 @@ impl Token {
         None
     }
 
-    fn expect_number(&self) -> Result<(i64, &Token)> {
+    fn expect_number(&self) -> Result<(i64, &Self)> {
         self.consume_number()
             .ok_or(CustomError("数字ではありません".to_string(), self.loc))
     }
 
-    fn is_eof(&self) -> bool {
-        return self.kind == TokenKind::EOF;
-    }
-
-    fn append(&mut self, kind: TokenKind, loc: Location) -> &mut Token {
+    fn append(&mut self, kind: TokenKind, loc: Location) -> &mut Self {
         let t = Token {
             kind,
             loc,
@@ -93,7 +89,7 @@ fn tokenise(p: &str) -> Result<Token> {
         let c = input.chars().nth(0).unwrap();
         let rest = match c {
             _ if c.is_ascii_whitespace() => &input[1..],
-            _ if c == '+' || c == '-' => {
+            _ if ['+', '-', '*', '/', '(', ')'].contains(&c) => {
                 cur = cur.append(
                     TokenKind::Reserved(c),
                     Location((p.chars().count() - input.chars().count()) as u64),
@@ -142,7 +138,7 @@ struct Node {
 }
 
 impl Node {
-    fn new(kind: NodeKind, lhs: Option<Node>, rhs: Option<Node>) -> Node {
+    fn new(kind: NodeKind, lhs: Option<Self>, rhs: Option<Self>) -> Self {
         Node {
             kind,
             lhs: lhs.map(|n| Box::new(n)),
@@ -150,7 +146,7 @@ impl Node {
         }
     }
 
-    fn new_number(n: i64) -> Node {
+    fn new_number(n: i64) -> Self {
         Node {
             kind: NodeKind::Num(n),
             lhs: None,
@@ -223,6 +219,32 @@ fn primary(token: &Token) -> Result<(Node, &Token)> {
         .map(|(n, next)| (Node::new_number(n), next))
 }
 
+fn gen(node: &Node) {
+    if let NodeKind::Num(n) = node.kind {
+        println!("  push {}", n);
+        return;
+    }
+
+    gen(&node.lhs.as_ref().unwrap());
+    gen(&node.rhs.as_ref().unwrap());
+
+    println!("  pop rdi");
+    println!("  pop rax");
+
+    match node.kind {
+        NodeKind::Add => println!("  add rax, rdi"),
+        NodeKind::Sub => println!("  sub rax, rdi"),
+        NodeKind::Mul => println!("  imul rax, rdi"),
+        NodeKind::Div => {
+            println!("  cqo");
+            println!("  idiv rdi");
+        }
+        _ => {}
+    }
+
+    println!("  push rax");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
@@ -239,38 +261,15 @@ fn main() {
 
 fn run(input: &str) -> Result<()> {
     let token = tokenise(input)?;
+    let (node, _) = expr(&token)?;
 
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
 
-    let (d, mut token) = token.expect_number()?;
-    println!("  mov rax, {}", d);
+    gen(&node);
 
-    while !token.is_eof() {
-        let (res, next) = token.expect_reserved()?;
-        let next = match res {
-            '+' => {
-                let (d, next) = next.expect_number()?;
-                println!("  add rax, {}", d);
-                next
-            }
-            '-' => {
-                let (d, next) = next.expect_number()?;
-                println!("  sub rax, {}", d);
-                next
-            }
-            _ => {
-                return Err(CustomError(
-                    format!("予期しない文字です: {}", res),
-                    token.loc,
-                ))
-            }
-        };
-
-        token = next;
-    }
-
+    println!("  pop rax");
     println!("  ret");
     Ok(())
 }
